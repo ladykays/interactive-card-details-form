644d83f722144e583ef4dbb365d651dcf461a714
Kerrion Siokwu <ladykerrion@sky.com>
2023-10-02 15:30:48 +0100

    add working date event handler, cvc event handler not populating yet

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+
+document.addEventListener('DOMContentLoaded', function () {
 const cardHolderNameInput = document.querySelector(".cardHolderName");
 const cardNumberInput = document.querySelector(".cardNumber");
+const monthInput = document.querySelector(".month");
+const yearInput = document.querySelector(".year"); 
+const expiryDate = document.querySelector(".expiry-date");
+const cvcInput = document.querySelector(".cvc");
 
+
+//Handler for cardHolderName field
 cardHolderNameInput.addEventListener("input", () => {
-  const cardHolderName = cardHolderNameInput.value; document.querySelector(".name").textContent = cardHolderName;
+  const cardHolderName = cardHolderNameInput.value; 
+  document.querySelector(".name").textContent = cardHolderName;
 });
 
+//Handler for CVC field
+cvcInput.addEventListener("input", () => {
+  const cvc = cvcInput.value;
+  document.querySelector("p.cvc").textContent = cvc;
+  console.log(cvc);
+});
+
+//Handler for cardNumber field
 cardNumberInput.addEventListener("input", () => {
   const cardNumber =cardNumberInput.value;
   document.querySelector(".card-no").textContent = cardNumber;
+  console.log(cardNumber);
 });
 
+//Handler for expiry date field
+monthInput.addEventListener("input", expiryDateInput);
+
+yearInput.addEventListener("input", expiryDateInput);
+
+function expiryDateInput() {
+  const month = monthInput.value;
+  const year = yearInput.value;
+  expiryDate.textContent = month + "/" + year;
+};
+
+
+
+});
